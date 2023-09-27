7e0b31ed6d3fd1aa3214c7478050d02c6d6e3dce
themuuln <zerone.offical@gmail.com>
2023-09-27 14:36:18 +0800

    changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect } from "react";
 
@@ -7,21 +7,27 @@ export default function Home() {
     const userAgent = navigator.userAgent;
 
     if (/android/i.test(userAgent)) {
-      window.location.href = 'https://play.google.com/store/apps/details?id=com.mindshift.usell';
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.mindshift.usell";
     } else if (/iPad|iPhone|iPod/i.test(userAgent)) {
       // Redirect to Apple App Store for iOS
-      window.location.href = 'https://apps.apple.com/id/app/mst-supply/id6447289799';
+      window.location.href =
+        "https://apps.apple.com/id/app/mst-supply/id6447289799";
     } else {
       // Redirect to a generic landing page for other devices
-      window.location.href = 'https://agrosupplytrade.mn/';
+      window.location.href = "https://agrosupplytrade.mn/";
     }
-  }
+  };
 
-  useEffect(() => {
-    redirectToAppStore();
-  }, []);
+  // useEffect(() => {
+  //   redirectToAppStore();
+  // }, []);
 
   return (
-    <h1>Please Wait</h1>
+    <>
+      <section className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-center">Please Wait ...</h1>
+      </section>
+    </>
   )
 }
